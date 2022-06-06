f55c6bc8d42682e14be6d526df38ad42513cdddc
bismarkstoney <bismarkstoney@yahoo.com>
2022-06-06 02:59:40 +0000

    change deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req,res)=>{
-    res.status(200).json({message: 'Hello world'})
+    res.status(200).json({message: 'additoinal deployment'})
 })
 
 app.listen(PORT, ()=>{
