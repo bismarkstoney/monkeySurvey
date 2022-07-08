b7f90758b94e0ed6ff88861feef51e21cc59d19e
bismarkstoney <bismarkstoney@yahoo.com>
2022-07-08 06:49:31 +0000

    setup serilize and deserialize functions

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config({});
 const express = require('express');
+const connectDB = require('./config/db/db');
+//Client ID
+//Client secret
+connectDB();
 //Not exporting anyting
 require('./lib/passport');
 const authRouter = require('./routes/authRouter');
-const coonnectDB = require('./config/db/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-//Client ID
-//Client secret
-coonnectDB();
 
 app.use('/auth/google', authRouter);
 app.listen(PORT, () => {
