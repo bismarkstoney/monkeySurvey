d96beba2394ff5903932e2467dc2b633acc2e586
bismarkstoney <bismarkstoney@yahoo.com>
2022-07-11 07:06:08 +0000

    auth done

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+	googleClientID:
+		'865012285445-9i61u3dfbc57l7lai2hhiab9gedkklmv.apps.googleusercontent.com',
+	googleClientSecret: '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',
+};
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLEINT_ID,
+
+	googleClientSecret: process.env.GOOGLE_CLEINT_SECRET,
+	COOKIE_KEY: process.env.COOKIE_KEY,
+	mongoURI: process.env.MONGO_URI,
+};
